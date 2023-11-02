58ae9225945cd149d2a646525dc12a90f834db1b
Collatera24 <kola.tella24@gmail.com>
2023-11-02 15:48:10 -0700

    a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,7 +21,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-		for (a = 0; s1[a]; ++a)
+		for (a = 0; s1[a] != '\0'; a++)
 			;
 	}
 	if (s2 == NULL)
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-		for (b = 0; s2[b]; ++b)
+		for (b = 0; s2[b] != '\0'; b++)
 			;
 	}
 	if (b > n)
@@ -38,9 +38,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	s = malloc(sizeof(char) * (a + b + 1));
 	if (s == NULL)
 		return (NULL);
-	for (c = 0; c > a; c++)
+	for (c = 0; c < a; c++)
 		s[c] = s1[c];
-	for (c = 0; c > b; c++)
+	for (c = 0; c < b; c++)
 		s[c + a] = s2[c];
 	s[a + b] = '\0';
 	return (s);
