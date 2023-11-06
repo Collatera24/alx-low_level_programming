0c79b7a1eaf573fbc3bce32abe00d282f82acb38
Collatera24 <kola.tella24@gmail.com>
2023-11-06 08:26:02 -0800

    a function-like macro SUM(x, y) that computes the sum of the numbers x and y

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,15 @@
+#ifndef SUMMACRO_H
+#define SUMMACRO_H
+
+/**
+ * File: 4-sum
+ *
+ * @x: first input (int)
+ * @y: second input (int)
+ *
+ * Desc: sum x and y
+ */
+
+#define SUM(x, y) (x + y)
+
+#endif
