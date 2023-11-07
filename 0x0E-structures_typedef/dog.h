98383b6fde6e397e56ae9ad50373a621170e06ac
Collatera24 <kola.tella24@gmail.com>
2023-11-06 16:33:38 -0800

    a function that prints a struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,12 +11,13 @@
  * Desc: struct with alx-low_programming
  */
 
-struct dog
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
